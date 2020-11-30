cfe5eef008bd780647bc3a3773115aa532742c46
fredrik1992 <klosansa1@.com>
2020-11-30 20:09:20 +0100

    added createElement function and made it possible to create notes

--- JS/index.js
+++ JS/index.js
@@ -4,22 +4,22 @@
 
 function createElementType(type/*  1 for note 2 for list*/){//creates a element with values depending on the type and adds to page 'Fredrik
     
-    var mainWindow = document.getElementById("test");
-    var newNoteElement = document.createElement("div");
+    var mainWindow = document.getElementById("placeholder");// add div wich is gonna hold all elements
+    var newElement = document.createElement("div");
     
     
 
-    newNoteElement.setAttribute("style","background-color: yellow; height: 200px; width:200px; margin:200px; text-align:center");
-    newNoteElement.setAttribute("height", "100px");
+    newElement.setAttribute("style","background-color: yellow; height: 200px; width:200px; margin:200px; text-align:center");
+    newElement.setAttribute("height", "100px");
     
 
     if(type ==1){
         var text = prompt("add text here")
-        newNoteElement.textContent = text;
+        newElement.textContent = text;
     }
 
 
-    mainWindow.appendChild(newNoteElement);
+    mainWindow.appendChild(newElement);
 
 
     
