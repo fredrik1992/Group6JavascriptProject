c89472e94cba60c31157aa1639793647c0254e97
fredrik1992 <klosansa1@.com>
2020-11-30 17:22:17 +0100

    added function to create element + to create note

--- JS/index.js
+++ JS/index.js
@@ -2,16 +2,21 @@
 
 
 
-function createNoteElement(){
+function createElementType(type/*  1 for note 2 for list*/){//creates a element with values depending on the type and adds to page 'Fredrik
     
-    var mainWindow = document.getElementById("Placeholder : put div id to add the element to");
+    var mainWindow = document.getElementById("test");
     var newNoteElement = document.createElement("div");
-    var text = prompt("write text")
+    
     
 
     newNoteElement.setAttribute("style","background-color: yellow; height: 200px; width:200px; margin:200px; text-align:center");
     newNoteElement.setAttribute("height", "100px");
-    newNoteElement.textContent = text;
+    
+
+    if(type ==1){
+        var text = prompt("add text here")
+        newNoteElement.textContent = text;
+    }
 
 
     mainWindow.appendChild(newNoteElement);
