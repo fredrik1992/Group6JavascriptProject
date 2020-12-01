e41458c7a9b9c3f6e7d0ab0000ae29d03f442016
fredrik1992 <klosansa1@.com>
2020-12-01 09:10:32 +0100

    changed propmt in note to textare

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,6 @@
 
 
 
-
 function createElementType(type/*  1 for note 2 for list*/){//creates a element with values depending on the type and adds to page 'Fredrik
     
     var mainWindow = document.getElementById("placeholder");// add div wich is gonna hold all elements
@@ -14,8 +13,9 @@ function createElementType(type/*  1 for note 2 for list*/){//creates a element 
     
 
     if(type ==1){
-        var text = prompt("add text here")
-        newElement.textContent = text;
+        var text = document.createElement("textarea")
+        text.setAttribute("style","background-color: yellow; height:100%; width:100% ")
+        newElement.appendChild(text)
     }
 
 
